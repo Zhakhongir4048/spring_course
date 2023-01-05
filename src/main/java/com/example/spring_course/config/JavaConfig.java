a2107def10bf87245931e66c076104e53948ceeb
Эсонов Жахонгир <Zhakhongir.Esonov@softline.com>
2023-01-05 20:35:16 +0300

    Ready

--- src/main/java/com/example/spring_course/config/JavaConfig.java
+++ src/main/java/com/example/spring_course/config/JavaConfig.java
@@ -2,6 +2,7 @@ package com.example.spring_course.config;
 
 import com.example.spring_course.spring_introduction.Dog;
 import com.example.spring_course.spring_introduction.Pet;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +13,7 @@ import org.springframework.context.annotation.Scope;
 public class JavaConfig {
 
   @Bean("myPet")
-  @Scope("singleton")
+  @Scope(BeanDefinition.SCOPE_SINGLETON)
   public Pet getPet() {
     return new Dog();
   }
