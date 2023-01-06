e1669796765b2f7611eafe1471e76c328b42f2c2
Эсонов Жахонгир <Zhakhongir.Esonov@softline.com>
2023-01-06 14:22:53 +0300

    Ready

--- src/main/java/com/example/spring_course/hibernate_one_to_many_uni/Test1.java
+++ src/main/java/com/example/spring_course/hibernate_one_to_many_uni/Test1.java
@@ -2,38 +2,36 @@ package com.example.spring_course.hibernate_one_to_many_uni;
 
 import com.example.spring_course.hibernate_one_to_many_uni.entity.Department;
 import com.example.spring_course.hibernate_one_to_many_uni.entity.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+@Slf4j
 public class Test1 {
 
-    private static final Logger log = LoggerFactory.getLogger(Test1.class);
-
-    public static void main(String[] args) {
-        log.info("Method main starts");
-        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
-                .addAnnotatedClass(Employee.class)
-                .addAnnotatedClass(Department.class)
-                .buildSessionFactory();
-        try (sessionFactory) {
-            Session currentSession = sessionFactory.getCurrentSession();
-            Department department = new Department("Flowers", 100000, 30000);
-            Employee emp1 = new Employee("Jasur", "Esonov", 120000);
-            Employee emp2 = new Employee("Uktam", "Esonov", 30000);
-            department.addEmployeeToDepartment(emp1);
-            department.addEmployeeToDepartment(emp2);
-            currentSession.beginTransaction();
-            currentSession.save(department);
-            currentSession.getTransaction().commit();
-            log.info("Success");
-        } catch (HibernateException e) {
-            e.printStackTrace();
-        }
-        log.info("Method main ends");
+  public static void main(String[] args) {
+    log.info("Method main starts");
+    SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+        .addAnnotatedClass(Employee.class)
+        .addAnnotatedClass(Department.class)
+        .buildSessionFactory();
+    try (sessionFactory) {
+      Session currentSession = sessionFactory.getCurrentSession();
+      Department department = new Department("Flowers", 100000, 30000);
+      Employee emp1 = new Employee("Jasur", "Esonov", 120000);
+      Employee emp2 = new Employee("Uktam", "Esonov", 30000);
+      department.addEmployeeToDepartment(emp1);
+      department.addEmployeeToDepartment(emp2);
+      currentSession.beginTransaction();
+      currentSession.save(department);
+      currentSession.getTransaction().commit();
+      log.info("Success");
+    } catch (HibernateException e) {
+      e.printStackTrace();
     }
+    log.info("Method main ends");
+  }
 
 }
--- src/main/java/com/example/spring_course/hibernate_one_to_many_uni/entity/Employee.java
+++ src/main/java/com/example/spring_course/hibernate_one_to_many_uni/entity/Employee.java
@@ -1,86 +1,57 @@
 package com.example.spring_course.hibernate_one_to_many_uni.entity;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "employees")
 public class Employee {
 
-    @Id
-    @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false)
+  private int id;
 
-    @Column(name = "name")
-    private String name;
+  @Column(name = "name")
+  private String name;
 
-    @Column(name = "surname")
-    private String surname;
+  @Column(name = "surname")
+  private String surname;
 
-    @Column(name = "salary")
-    private int salary;
+  @Column(name = "salary")
+  private int salary;
 
-    @ManyToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "department_id")
-    private Department department;
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "department_id")
+  private Department department;
 
-    public Employee() {
-    }
+  public Employee(String name, String surname, int salary) {
+    this.name = name;
+    this.surname = surname;
+    this.salary = salary;
+  }
 
-    public Employee(String name, String surname, int salary) {
-        this.name = name;
-        this.surname = surname;
-        this.salary = salary;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getSalary() {
-        return salary;
-    }
-
-    public void setSalary(int salary) {
-        this.salary = salary;
-    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    @Override
-    public String toString() {
-        return "Employee{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", salary=" + salary +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", surname='" + surname + '\'' +
+        ", salary=" + salary +
+        '}';
+  }
 
 }
